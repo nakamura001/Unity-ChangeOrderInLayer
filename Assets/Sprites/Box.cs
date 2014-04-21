e18260361b2a2bf115ff0f540631fc2e34f3fe52
nakamura001 <polluxtk@gmail.com>
2014-04-21 21:12:25 +0900

    Initial commit.

---
+++ Assets/Sprites/Box.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PolygonCollider2D))]
+public class Box : MonoBehaviour {
+	Vector3 dragOffset;
+	Manager manager;
+
+	void Start () {
+		manager = transform.parent.gameObject.GetComponent<Manager>();
+	}
+	
+	void OnMouseDown() {
+		manager.ChangeForegroundRenderer(gameObject.renderer.sortingOrder);
+		dragOffset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+	}
+	
+	void OnMouseDrag() {
+		Vector3 screenPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+		transform.position = screenPoint + dragOffset;
+	}
+}
---
+++ Assets/Sprites/Manager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+	Box[] boxArray;
+
+	void Start () {
+		boxArray = gameObject.GetComponentsInChildren<Box>();
+		int count=0;
+		foreach(Box box in boxArray) {
+			box.renderer.sortingOrder = count++;
+		}
+	}
+	
+	public void ChangeForegroundRenderer(int targetOrder) {
+		foreach(Box box in boxArray) {
+			if (targetOrder == box.renderer.sortingOrder) {
+				box.renderer.sortingOrder = boxArray.Length - 1;
+			} else if (targetOrder < box.renderer.sortingOrder) {
+				box.renderer.sortingOrder--;
+			}
+		}
+	}
+}
